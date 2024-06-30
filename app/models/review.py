013f644749c2eff8966b67010f47131fb125e1d9
Hui Wen <lovelyelsa@macbook-air-10.lan>
2024-06-30 12:50:31 -0700

    changes

--- app/models/review.py
+++ app/models/review.py
@@ -6,8 +6,8 @@ reviews = db.Table(
 
     db.Model.metadata,
 
-    db.Column('product_id', db.Integer, db.ForeignKey(add_prefix_for_prod('products.id')), primary_key=True),
-    db.Column('user_id', db.Integer,db.ForeignKey(add_prefix_for_prod('users.id')), primary_key=True),
+    db.Column('product_id', db.Integer, db.ForeignKey(add_prefix_for_prod('products.id'), ondelete='CASCADE'), primary_key=True),
+    db.Column('user_id', db.Integer,db.ForeignKey(add_prefix_for_prod('users.id'), ondelete='CASCADE'),primary_key=True),
     db.Column('review', db.String(2000), nullable=False),
     db.Column('stars', db.Integer, nullable=False),
     db.Column('created_at', db.DateTime, default=datetime.utcnow),
