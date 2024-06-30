56667f998a63390a07537386f6fcab07d2f75fc0
Hui Wen <lovelyelsa@macbook-air-10.lan>
2024-06-30 13:57:33 -0700

    whatever

--- app/models/order_items.py
+++ app/models/order_items.py
@@ -2,7 +2,7 @@ from .db import db, environment, SCHEMA, add_prefix_for_prod
 
 order_item_association = db.Table(
     "order_items",
-    db.Column('order_id', db.Integer, db.ForeignKey(add_prefix_for_prod('orders.id')), primary_key = True),
-    db.Column('product_id', db.Integer, db.ForeignKey(add_prefix_for_prod('products.id')), primary_key = True),
+    db.Column('order_id', db.Integer, db.ForeignKey(add_prefix_for_prod('orders.id'),ondelete='CASCADE'), primary_key = True),
+    db.Column('product_id', db.Integer, db.ForeignKey(add_prefix_for_prod('products.id'), ondelete='CASCADE'), primary_key = True),
     db.Column('quantity', db.Integer)
 )
--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -9,12 +9,6 @@ from .order_items import seed_order_items, undo_order_items
 
 from app.models.db import db, environment, SCHEMA
 
-# Ensure the schema exists
-def create_schema():
-    if environment == 'production':
-        db.session.execute(f"CREATE SCHEMA IF NOT EXISTS {SCHEMA}")
-        db.session.commit()
-
 # Creates a seed group to hold our commands
 # So we can type `flask seed --help`
 seed_commands = AppGroup('seed')
@@ -24,13 +18,12 @@ seed_commands = AppGroup('seed')
 @seed_commands.command('all')
 def seed():
     if environment == 'production':
-        create_schema()
         # Before seeding in production, you want to run the seed undo
         # command, which will  truncate all tables prefixed with
         # the schema name (see comment in users.py undo_users function).
         # Make sure to add all your other model's undo functions below
-        undo_orders()
         undo_order_items()
+        undo_orders()
         undo_shopping_cart_items()
         undo_reviews()
         undo_product_images()
@@ -50,8 +43,8 @@ def seed():
 # Creates the `flask seed undo` command
 @seed_commands.command('undo')
 def undo():
-    undo_orders()
     undo_order_items()
+    undo_orders()
     undo_shopping_cart_items()
     undo_reviews()
     undo_product_images()
